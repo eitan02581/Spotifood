5b9709840cb9996f15801b93064869fbbdebc3e2
yeruhin <yeruhin@gmail.com>
2019-03-20 17:54:51 +0200

    a

--- frontend/src/store/group-store.js
+++ frontend/src/store/group-store.js
@@ -1,21 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-<<<<<<< HEAD
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
- 
-  }
-})
-=======
 
 Vue.use(Vuex)
 
@@ -37,4 +22,3 @@ const groupStore = {
 }
 
 export default groupStore
->>>>>>> 8eac5727ddcfb25ed0570e7afdef52b357bbac1c

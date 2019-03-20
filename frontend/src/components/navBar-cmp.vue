fc7d302a2fc83853f743a4204a829b70010d4b1e
Eitan Elnekave <eitan02581@gmail.com>
2019-03-20 08:46:07 +0200

    first commit

---
+++ frontend/src/components/navBar-cmp.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="navBar-warpper">
+    <div class="nav-container"></div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scpoed lang="scss">
+.navBar-warpper {
+  width: 100%;
+  height: 130px;
+  background-color: burlywood;
+}
+</style>

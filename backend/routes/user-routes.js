17f2cd20d5be0b1b5ee7a7da19f2c9ae8e6c17f9
Yovel Becker <yovelb13@gmail.com>
2019-03-21 10:32:06 +0200

    full crud backend with db for recipes

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -7,6 +7,14 @@ function addUserRoutes(app){
         return res.json(UserService.query())
     })
 
+    app.get('/user/:userId', (req, res) => {
+        const userId = req.params.userId
+        UserService.getById(userId)
+            .then(user => {
+                console.log('user by id got', user)
+                res.json(user)
+            })
+    })
 }
 
 module.exports = addUserRoutes

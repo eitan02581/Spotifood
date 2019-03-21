1c7a40539ac5d9287bda8e627dd5d4894c5f7aec
yeruhin <yeruhin@gmail.com>
2019-03-21 12:57:51 +0200

    details without bugs

--- frontend/src/services/RecipeService.js
+++ frontend/src/services/RecipeService.js
@@ -4,10 +4,7 @@ import axios from 'axios'
 const RECIPE_ROUTE = (process.env.NODE_ENV !== 'development') ? '/recipe' : 'http://localhost:3003/recipe'
 
 function getRecipeById(recipeId){
-    console.log('recipe id',recipeId)
-    return axios.get(`${RECIPE_ROUTE}/${recipeId}`).then(recipe => {
-        console.log('recipe data',recipe.data)
-        return recipe.data})
+    return axios.get(`${RECIPE_ROUTE}/${recipeId}`).then(recipe => recipe.data)
 }
 
 export default {

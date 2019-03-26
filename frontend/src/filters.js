9dabc383ff80ca8f85779eb4b6f0f51c58662e17
yeruhin <yeruhin@gmail.com>
2019-03-26 14:57:13 +0200

    profile page

--- frontend/src/filters.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import moment from 'moment'
-
-Vue.filter('time', (timestamp) => {
-    return moment(timestamp).fromNow();
-})
-
-
-
--- frontend/src/services/EventBusService.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue';
-export const eventBus = new Vue();
-export const USER_LOGGED = 'user is logged';

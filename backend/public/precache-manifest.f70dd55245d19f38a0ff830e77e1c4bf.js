1a8e639f728ae8f6bd5fa68156b835413026c44d
Yovel Becker <yovelb13@gmail.com>
2019-03-28 00:26:53 +0200

    try heroku

--- backend/public/precache-manifest.f70dd55245d19f38a0ff830e77e1c4bf.js
+++ backend/public/precache-manifest.f70dd55245d19f38a0ff830e77e1c4bf.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "28042793f45c99d2ae5316211fa25e30",
-    "url": "/img/calender.28042793.svg"
+    "revision": "87c3edb47bd543f133aba14af88e9752",
+    "url": "/fonts/Righteous-Regular.87c3edb4.ttf"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/robots.txt"
   },
   {
-    "revision": "6c2c45fe8853c8028a0d2848b2d4b9e7",
-    "url": "/img/clock.6c2c45fe.svg"
+    "revision": "ec4c9962ba54eb91787aa93d361c10a8",
+    "url": "/fonts/Roboto-Black.ec4c9962.ttf"
   },
   {
     "revision": "928ea53ae60989efad5c",
     "url": "/css/chunk-vendors.0a391102.css"
   },
   {
-    "revision": "6e13f51ad23f477b52eda0c26e6e13ca",
-    "url": "/img/beer.6e13f51a.jpg"
+    "revision": "3b265d437ec1c3b3bce81affda06605c",
+    "url": "/img/table.3b265d43.jpg"
   },
   {
     "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
@@ -36,28 +36,24 @@ self.__precacheManifest = [
     "url": "/img/cheers.ef48c55a.svg"
   },
   {
-    "revision": "3b265d437ec1c3b3bce81affda06605c",
-    "url": "/img/table.3b265d43.jpg"
-  },
-  {
     "revision": "fe35890a50ecab68a3bc533d5331f396",
     "url": "/img/table4.fe35890a.jpg"
   },
   {
-    "revision": "9505baad814ac152b63e",
-    "url": "/js/app.b68261b1.js"
+    "revision": "28042793f45c99d2ae5316211fa25e30",
+    "url": "/img/calender.28042793.svg"
+  },
+  {
+    "revision": "6c2c45fe8853c8028a0d2848b2d4b9e7",
+    "url": "/img/clock.6c2c45fe.svg"
+  },
+  {
+    "revision": "9b4d377fde5351d9b0a5",
+    "url": "/js/app.c3a07058.js"
   },
   {
     "revision": "928ea53ae60989efad5c",
     "url": "/js/chunk-vendors.09db4ccb.js"
-  },
-  {
-    "revision": "87c3edb47bd543f133aba14af88e9752",
-    "url": "/fonts/Righteous-Regular.87c3edb4.ttf"
-  },
-  {
-    "revision": "ec4c9962ba54eb91787aa93d361c10a8",
-    "url": "/fonts/Roboto-Black.ec4c9962.ttf"
   },
   {
     "revision": "32f5f0193e37f44cc85537968ea487c1",
@@ -72,6 +68,10 @@ self.__precacheManifest = [
     "url": "/media/Group-Music.d2ed8175.mp4"
   },
   {
+    "revision": "e88410a665b7c31debcbe81eec48780b",
+    "url": "/img/Picture1.e88410a6.png"
+  },
+  {
     "revision": "ad0d45cf68ebc9983c81fe0b69b79021",
     "url": "/img/shef.ad0d45cf.svg"
   },
@@ -80,15 +80,19 @@ self.__precacheManifest = [
     "url": "/img/guests.e377e3a3.svg"
   },
   {
+    "revision": "6e13f51ad23f477b52eda0c26e6e13ca",
+    "url": "/img/beer.6e13f51a.jpg"
+  },
+  {
     "revision": "68e34fed8533e48a8b82909afc7a1524",
     "url": "/img/map.68e34fed.gif"
   },
   {
-    "revision": "f05def7e093a0e0751134cf2e22cab4b",
+    "revision": "433251216a7745d7039ca6a191d48e02",
     "url": "/index.html"
   },
   {
-    "revision": "9505baad814ac152b63e",
-    "url": "/css/app.6f630e9b.css"
+    "revision": "9b4d377fde5351d9b0a5",
+    "url": "/css/app.b9823e0e.css"
   }
 ];

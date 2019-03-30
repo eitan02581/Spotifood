95ac3a51765316f29416e8c943b7946759870dde
Yovel Becker <yovelb13@gmail.com>
2019-03-30 16:53:28 +0300

    workin homepage

--- frontend/src/views/homePage.vue
+++ frontend/src/views/homePage.vue
@@ -49,39 +49,27 @@
           <cuisineTypes @filterBy="onFilter"></cuisineTypes>
         </div>
         <div class="groups-previews">
-          <h1>Breakfast</h1>
-          <div class="group-container">
-            <GroupPreview v-for="group in breakfastGroups" :key="group._id" :group="group"></GroupPreview>
-          </div>
-          <h1>Lunch</h1>
-          <div class="group-container">
-            <GroupPreview v-for="group in lunchGroups" :key="group._id" :group="group"></GroupPreview>
-          </div>
-          <h1>Dinner</h1>
-          <div class="group-container">
-            <GroupPreview v-for="group in dinnerGroups" :key="group._id" :group="group"></GroupPreview>
-            <div class="br-wrap">
-              <h1 @click="onFilter( 'eventType', 'Breakfast')">Breakfast</h1>
-              <div class="group-container br-con">
-                <GroupPreview v-for="group in breakfastGroups" :key="group._id" :group="group"></GroupPreview>
-                <!-- <GroupList :groups="groups"></GroupList> -->
-              </div>
+          <div class="br-wrap">
+            <h1 @click="onFilter( 'eventType', 'Breakfast')">Breakfast</h1>
+            <div class="group-container br-con">
+              <GroupPreview v-for="group in breakfastGroups" :key="group._id" :group="group"></GroupPreview>
+              <!-- <GroupList :groups="groups"></GroupList> -->
             </div>
-            <!-- <hr> -->
-            <div class="ln-wrap">
-              <h1 @click="onFilter( 'eventType', 'Lunch')">Lunch</h1>
-              <div class="group-container">
-                <GroupPreview v-for="group in lunchGroups" :key="group._id" :group="group"></GroupPreview>
-                <!-- <GroupList :groups="groups"></GroupList> -->
-              </div>
+          </div>
+          <!-- <hr> -->
+          <div class="ln-wrap">
+            <h1 @click="onFilter( 'eventType', 'Lunch')">Lunch</h1>
+            <div class="group-container">
+              <GroupPreview v-for="group in lunchGroups" :key="group._id" :group="group"></GroupPreview>
+              <!-- <GroupList :groups="groups"></GroupList> -->
             </div>
-            <!-- <hr> -->
-            <div class="din-wrap">
-              <h1 @click="onFilter( 'eventType', 'Dinner')">Dinner</h1>
-              <div class="group-container">
-                <GroupPreview v-for="group in dinnerGroups" :key="group._id" :group="group"></GroupPreview>
-                <!-- <GroupList :groups="groups"></GroupList> -->
-              </div>
+          </div>
+          <!-- <hr> -->
+          <div class="din-wrap">
+            <h1 @click="onFilter( 'eventType', 'Dinner')">Dinner</h1>
+            <div class="group-container">
+              <GroupPreview v-for="group in dinnerGroups" :key="group._id" :group="group"></GroupPreview>
+              <!-- <GroupList :groups="groups"></GroupList> -->
             </div>
           </div>
         </div>
@@ -117,19 +105,19 @@ export default {
     };
   },
   async created() {
-    let a = 1552243874000
-    let b = 1554918674000
-    console.log(b-a)
+    let a = 1552243874000;
+    let b = 1554918674000;
+    console.log(b - a);
     await this.$store.dispatch({ type: "getGroups" });
     this.breakfastGroups = this.$store.getters.groups.filter(
       group => group.eventType === "Breakfast"
-    );
+    ).slice(0, 7);
     this.lunchGroups = this.$store.getters.groups.filter(
       group => group.eventType === "Lunch"
-    );
+    ).slice(0, 7);
     this.dinnerGroups = this.$store.getters.groups.filter(
       group => group.eventType === "Dinner"
-    );
+    ).slice(0, 7);
     this.soonGroups = this.$store.getters.groups.filter(
       group =>
         group.time - new Date().getTime() < this.timeDiff &&
